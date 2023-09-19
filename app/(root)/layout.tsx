245f8b64ef6c715bd2fbd5c13e0fed807fb029a4
Caio Silva <caiossantosp5560@gmail.com>
2023-09-19 11:04:13 -0300

    feat: adicionar HomePage e suas estrututuras, autenticação com o clerk, responsividade

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,19 @@
 import '../globals.css'
 import type { Metadata } from 'next'
+
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from 'next/font/google'
 
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Bottombar from '@/components/shared/Bottombar';
+
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Threads',
-  description: 'Threads Clone',
+  description: 'A Next.js 13 Meta Threads Application',
 }
 
 export default function RootLayout({
@@ -18,7 +24,22 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="pt-br">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className='flex flex-row'>
+            <LeftSidebar />
+
+            <section className='main-container '>
+              <div className='w-full max-w-4xl'>
+                {children}
+              </div>
+            </section>
+            
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
